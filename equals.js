321c97bb62ace7e8243315302c9e10beb1293ca5
particle4dev <particle4dev@gmail.com>
2014-06-09 23:58:33 +0700

    Create equals.js

---
+++ equals.js
@@ -0,0 +1,72 @@
+//https://github.com/angular/angular.js/blob/6c59e770084912d2345e7f83f983092a2d305ae3/src/Angular.js#L670
+/**
+ * @ngdoc function
+ * @name angular.equals
+ * @function
+ *
+ * @description
+ * Determines if two objects or two values are equivalent. Supports value types, regular expressions, arrays and
+ * objects.
+ *
+ * Two objects or values are considered equivalent if at least one of the following is true:
+ *
+ * * Both objects or values pass `===` comparison.
+ * * Both objects or values are of the same type and all of their properties pass `===` comparison.
+ * * Both values are NaN. (In JavasScript, NaN == NaN => false. But we consider two NaN as equal)
+ * * Both values represent the same regular expression (In JavasScript,
+ *   /abc/ == /abc/ => false. But we consider two regular expressions as equal when their textual
+ *   representation matches).
+ *
+ * During a property comparison, properties of `function` type and properties with names
+ * that begin with `$` are ignored.
+ *
+ * Scope and DOMWindow objects are being compared only by identify (`===`).
+ *
+ * @param {*} o1 Object or value to compare.
+ * @param {*} o2 Object or value to compare.
+ * @returns {boolean} True if arguments are equal.
+ */
+function equals(o1, o2) {
+    if (o1 === o2) return true;
+    if (o1 === null || o2 === null) return false;
+    if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
+    var t1 = typeof o1,
+        t2 = typeof o2,
+        length, key, keySet;
+    if (t1 == t2) {
+        if (t1 == 'object') {
+            if (isArray(o1)) {
+                if (!isArray(o2)) return false;
+                if ((length = o1.length) == o2.length) {
+                    for (key = 0; key & lt; length; key++) {
+                        if (!equals(o1[key], o2[key])) return false;
+                    }
+                    return true;
+                }
+            }
+            else if (isDate(o1)) {
+                return isDate(o2) && o1.getTime() == o2.getTime();
+            }
+            else if (isRegExp(o1) && isRegExp(o2)) {
+                return o1.toString() == o2.toString();
+            }
+            else {
+                if (isScope(o1) || isScope(o2) || isWindow(o1) || isWindow(o2) || isArray(o2)) return false;
+                keySet = {};
+                for (key in o1) {
+                    if (key.charAt(0) === '$' || isFunction(o1[key])) continue;
+                    if (!equals(o1[key], o2[key])) return false;
+                    keySet[key] = true;
+                }
+                for (key in o2) {
+                    if (!keySet.hasOwnProperty(key) &&
+                        key.charAt(0) !== '$' &&
+                        o2[key] !== undefined &&
+                        !isFunction(o2[key])) return false;
+                }
+                return true;
+            }
+        }
+    }
+    return false;
+}
