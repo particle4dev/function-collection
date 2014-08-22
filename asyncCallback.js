535c7481a074d403963cb7eb07034d99443b5f0f
particle4dev <particle4dev@gmail.com>
2014-08-22 16:28:25 +0700

    Update asyncCallback.js

--- asyncCallback.js
+++ asyncCallback.js
@@ -1,1 +1,38 @@
 // http://jsbin.com/wumuko/1/edit
+AsyncCallback = function(){
+  this._instance = null;
+  this._waitList = [];
+};
+_.extend(AsyncCallback.prototype, {
+  constructor: AsyncCallback,
+  setInstance: function(ins){
+    if(!_.isObject(ins))
+      throw new Error('instance must be a object');
+    this._instance = ins;
+  },
+  run: function(func){
+    if(!this._instance)
+      return this._waitList.push(func);
+    this._bind(func)();
+  },
+  _bind: function(func){
+    return _.bind(func, this._instance);
+  },
+  flush: function(){
+    if(this._instance){
+      var self = this;
+      _.each(this._waitList, function(func){
+        self._bind(func)();
+      });
+      this.resetWaitList();
+    }
+  },
+  resetWaitList: function(){
+    // reset
+    this._waitList = [];
+  },
+  runLastWaitList: function(){
+    var func = this._waitList.pop();
+    this._bind(func)();
+  }
+});
