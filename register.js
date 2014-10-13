247544e719f97f99c6d72184868a0e4a27ddd963
Le Hoang <particle4dev@gmail.com>
2014-10-14 01:27:18 +0700

    Update register.js

--- register.js
+++ register.js
@@ -1,16 +1,25 @@
 Register = (function () { 
     return function () {
-        var list = [];
+        var list = {}, arr = [];
         this.set = function (name, obj) {
             if(list[name])
                 throw new Error(name + ' is exists');
             list[name] = obj;
+            arr.push(obj);
         };
         this.get = function (name) {
-            return list[name];
+            if(_.isString(name))
+                return list[name];
+            return arr[name];
         };
         this.isSet = function (name) {
             return !!list[name];
+        };
+        this.length = function () {
+            return arr.length;
+        };
+        this.forEach = function (func) {
+            _.each(list, func);
         };
     };
 })();
