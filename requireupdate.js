2afeec10583650a97f6283b5d57546c678cc71b1
Le Hoang <particle4dev@gmail.com>
2015-03-08 11:47:20 +0700

    Update requireupdate.js

--- requireupdate.js
+++ requireupdate.js
@@ -1,17 +1,22 @@
 RequireUpdate = function(callback){
     var willUpdate = false;
+    var delay = 0;
     var cb = function(args){
         callback.apply(null, args);
-        willUpdate = false;
+        setTimeout(function () {
+            willUpdate = false;
+        }, delay);
     };
     this.run = function(number){
         if(_.isUndefined(number) || _.isNull(number))
-            number = 0;
+            delay = 0;
+        else
+            delay = number;
         if (! willUpdate) {
-            var args = Array.prototype.slice.call(arguments);
+            var args = Array.prototype.slice.call(arguments, 1);
             var c = cb.bind(this, args);
-            setTimeout(c, number);
             willUpdate = true;
+            c();
         }
     };
 };
